adadb61491a8473f5aaa6773287c658373f4608c
PKG-7 <pavel.k.group@gmail.com>
2022-08-01 13:09:58 +0300

    starting

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import Section from "./Section";
+
+export default function Home() {
+  return (
+    <Container>
+        <Section />
+        Home
+    </Container>
+  )
+}
+
+
+const Container = styled.div
+//     height: 100vh;
